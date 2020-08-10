e3614bf77398aebe19b63c26f572b15923653150
Antoine Bernelin <antoine.bernelin@studio-otsu.fr>
2020-08-10 23:10:51 +0200

    Command say. Cleanup.

---
+++ SineahBot/Commands/CommandSay.cs
@@ -0,0 +1,35 @@
+﻿using SineahBot.Data;
+using SineahBot.Interfaces;
+using SineahBot.Tools;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SineahBot.Commands
+{
+    public class CommandSay : Command
+    {
+
+        public CommandSay()
+        {
+            commandRegex = new Regex(@"^(talk |say |t |"" ?)(.+)$", RegexOptions.IgnoreCase);
+        }
+
+        public override void Run(IAgent agent, Room room)
+        {
+            var speach = GetArgument(2);
+
+            if (String.IsNullOrWhiteSpace(speach))
+            {
+                agent.Message("What are you trying to say ?");
+            }
+            else
+            {
+                room.DescribeAction($@"**{agent.name}** said: ""{speach}""", agent);
+                agent.Message($@"You said: ""{speach}""");
+                if (agent is Character) (agent as Character).experience += 1;
+            }
+        }
+    }
+}
