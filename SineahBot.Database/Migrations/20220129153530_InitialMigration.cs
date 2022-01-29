d87b54f04dafe6d2b61431186c26818fbb660de3
Antoine Bernelin <antoine.bernelin@studio-otsu.fr>
2022-01-29 15:36:12 +0000

    Initial Migration

---
+++ SineahBot.Database/Migrations/20220129153530_InitialMigration.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SineahBot.Database.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Characters",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Gender = table.Column<string>(type: "TEXT", nullable: false),
+                    Pronouns = table.Column<string>(type: "TEXT", nullable: false),
+                    CharacterClass = table.Column<string>(type: "TEXT", nullable: true),
+                    Level = table.Column<int>(type: "INTEGER", nullable: false),
+                    Experience = table.Column<int>(type: "INTEGER", nullable: false),
+                    Gold = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Characters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CharacterEquipment",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ItemName = table.Column<string>(type: "TEXT", nullable: true),
+                    IdCharacter = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharacterEquipment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CharacterEquipment_Characters_IdCharacter",
+                        column: x => x.IdCharacter,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CharacterItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ItemName = table.Column<string>(type: "TEXT", nullable: true),
+                    StackSize = table.Column<int>(type: "INTEGER", nullable: false),
+                    IdCharacter = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharacterItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CharacterItems_Characters_IdCharacter",
+                        column: x => x.IdCharacter,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CharacterMessages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    IdRoom = table.Column<string>(type: "TEXT", nullable: true),
+                    Message = table.Column<string>(type: "TEXT", nullable: true),
+                    IdCharacter = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharacterMessages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CharacterMessages_Characters_IdCharacter",
+                        column: x => x.IdCharacter,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    UserId = table.Column<ulong>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IdCharacter = table.Column<Guid>(type: "TEXT", nullable: true),
+                    Settings = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_Players_Characters_IdCharacter",
+                        column: x => x.IdCharacter,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CharacterEquipment_IdCharacter",
+                table: "CharacterEquipment",
+                column: "IdCharacter");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CharacterItems_IdCharacter",
+                table: "CharacterItems",
+                column: "IdCharacter");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CharacterMessages_IdCharacter",
+                table: "CharacterMessages",
+                column: "IdCharacter");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_IdCharacter",
+                table: "Players",
+                column: "IdCharacter",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CharacterEquipment");
+
+            migrationBuilder.DropTable(
+                name: "CharacterItems");
+
+            migrationBuilder.DropTable(
+                name: "CharacterMessages");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Characters");
+        }
+    }
+}
